2f3ad616636f47f6b747d28a12b0bbc77982c79f
IT2030DSatnic <S01031109@acad.tri-c.edu>
2018-04-15 16:22:30 -0400

    Lab 10 - got the browse page working and a layout that kind of looks like the picture in the instructions

---
+++ MVCMusicStoreApplication/Controllers/StoreController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCMusicStoreApplication.Models;
+
+namespace MVCMusicStoreApplication.Controllers
+{
+    public class StoreController : Controller
+    {
+        private MVCMusicStoreDB db = new MVCMusicStoreDB();
+
+        // GET: Store/Browse
+        public ActionResult Browse()
+        {
+            var genres = db.Genres.ToList();
+            return View(genres);
+        }
+
+        // GET: 
+        public ActionResult Index()
+        {
+            //return View();
+            return null;
+        }
+
+        // GET: 
+        public ActionResult Details()
+        {
+            //return View();
+            return null;
+        }
+    }
+}
